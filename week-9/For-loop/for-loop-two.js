464c23b9d2791f80a03c89b3004fe74c593646b5
bguemos <bri.lynnguemos@gmail.com>
2022-11-04 11:40:54 -0700

    new files and folders

---
+++ week-9/For-loop/for-loop-two.js
@@ -0,0 +1,17 @@
+var text = "";
+var total = 0;
+
+for(var i = 0; i < 6; i ++){
+if( i == 3) {
+    text = "finished";
+    break;
+
+} else if(i ==5) {
+    i++;
+} else {
+    //
+}
+total += i;
+}
+
+console.log(text + " at the number " + i + " with total of " + total);
